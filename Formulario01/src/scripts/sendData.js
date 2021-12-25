94e9282e2ba95e1375cd42fbee5fb8716a852e80
Marcus Davi S. Queiros <marcusdavi.soar@gmail.com>
2021-12-24 21:43:24 -0300

    Create sendData.js

---
+++ Formulario01/src/scripts/sendData.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('#sendEmail');
+console.log('Funfou');
+
+function mapObject() {
+    const person = {
+        firstname: document.querySelector('#nome').value,
+        lastname: document.querySelector('#sobrenome').value,
+        email: document.querySelector('#email').value,
+        cpf: document.querySelector('#cpf').value,
+        phone: document.querySelector('#number').value
+    }
+    return person;
+}
+
+async function submit(e) {
+    console.log(mapObject());
+    let obj = mapObject();
+    
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    };
+    e.preventDefault();
+    const response = await fetch('http://localhost:5000/send-email', options);
+    //console.log(options.body);
+}
+
+function printar() {
+    console.log('Hello world!');
+}
+
+btn.addEventListener('click', printar);
