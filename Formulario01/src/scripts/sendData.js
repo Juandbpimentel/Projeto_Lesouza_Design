8964ae0fcf7ee55272df1936bb0c225062cdfa9b
Marcus Davi S. Queiros <marcusdavi.soar@gmail.com>
2021-12-28 21:11:39 -0300

    Update sendData.js

--- Formulario01/src/scripts/sendData.js
+++ Formulario01/src/scripts/sendData.js
@@ -1,10 +1,23 @@
+let pkg;
+function getPackage() {
+    try {
+        pkg = document.querySelector('input[name="package"]:checked').value;
+        window.location.hash = '#form';
+    } catch(error) {
+        console.error(error);
+        window.alert('Por favor, selecione um dos pacotes!')
+        window.location.hash = '';
+    }
+}
+
 function mapObject() {
     const person = {
         firstname: document.querySelector('#nome').value,
         lastname: document.querySelector('#sobrenome').value,
         email: document.querySelector('#email').value,
         cpf: document.querySelector('#cpf').value,
-        phone: document.querySelector('#number').value
+        phone: document.querySelector('#number').value,
+        package: pkg
     }
     return person;
 }
